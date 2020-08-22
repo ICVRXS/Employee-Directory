50d360ca01d32fb6efc2db8624154358a16165ce
unknown <calebhay@rocketmail.com>
2020-08-22 09:45:17 -0700

    Server and package set up for Heroku deployment

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require ("express");
-const path = require ("path");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = process.env.PORT || 8080;
-const publicPath = path.join(__dirname, "../employeedirectory", "build");
 
-app.use(express.static(publicPath));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(publicPath, "index.html"));
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
